d5f26482b0923c7cd81c159e682cee284ebc7b96
sf71316 <111111>
2015-05-23 16:07:10 +0800

    CRUD Training

--- EF_Training/Course.cs
+++ EF_Training/Course.cs
@@ -22,9 +22,9 @@ namespace EF_Training_1
     
         public int CourseID { get; set; }
         public string Title { get; set; }
-        public int Credits { get; set; }
         public int DepartmentID { get; set; }
         public Nullable<int> CreatorID { get; set; }
+        public int Credits { get; set; }
     
         public virtual Department Department { get; set; }
         public virtual ICollection<Enrollment> Enrollment { get; set; }
--- EF_Training/Program.cs
+++ EF_Training/Program.cs
@@ -12,19 +12,49 @@ namespace EF_Training_1
         {
             using (var db =new ContosoUniversityEntities())
             {
-                foreach (var item in db.Course)
-                {
-                    Console.WriteLine(item.Title);
-                }
-                Console.WriteLine();
-                foreach (var item in db.Department)
-                {
-                    Console.WriteLine(item.Name);
-                    foreach (var c in item.Course)
-                    {
-                        Console.WriteLine("\t"+c.Title);
-                    }
-                }
+                #region Trainig Select
+                //foreach (var item in db.Course)
+                //{
+                //    Console.WriteLine(item.Title);
+                //}
+                //Console.WriteLine();
+                //foreach (var item in db.Department)
+                //{
+                //    Console.WriteLine(item.Name);
+                //    foreach (var c in item.Course)
+                //    {
+                //        Console.WriteLine("\t"+c.Credits);
+                //    }
+                //}
+                #endregion
+
+                #region Training Update
+                //foreach (var item in db.Course)
+                //{
+                //    item.Title += "!";
+                //}
+                //db.SaveChanges();
+                #endregion
+
+                #region Training Insert
+                //db.Course.Add(new Course { 
+                //    Title="Entity Framework 1",
+                //    Credits=1,
+                //    DepartmentID=2
+                //});
+                //db.Course.Add(new Course
+                //{
+                //    Title = "Entity Framework 6",
+                //    Credits = 1,
+                //    Department = db.Department.Find(1)
+                //});
+                //db.SaveChanges();
+                #endregion
+
+                #region Training Delete
+                db.Course.Remove(db.Course.FirstOrDefault(p => p.CourseID == 1));
+                db.SaveChanges();
+                #endregion
             }
         }
     }
