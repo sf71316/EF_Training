e1dc96e4f0c30b2b52dea5329bf46f1513719d17
sf71316 <111111>
2015-05-23 11:38:30 +0800

    modify

--- EF_Training/Program.cs
+++ EF_Training/Program.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EF_Training
+namespace EF_Training_1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (var db =new ContosoUniversityEntities())
+            {
+                foreach (var item in db.Course)
+                {
+                    Console.WriteLine(item.Title);
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
